24a1ec7131183efabc1d5243cb15c811f6d1f15e
Matthew Grover <mbgrover@smu.edu>
2022-09-16 21:02:57 -0500

    extra comments
    added comments to main for clarity

--- pdftotext.py
+++ pdftotext.py
@@ -10,27 +10,32 @@ pdfFileObj = open('file.pdf', 'rb')
 # create a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
+# create a list of characters to look for in the PDF (CASE SENSITIVE)
 characterList = []
 characterList.append("LORIN")
 characterList.append("NAN")
 characterList.append("MILES")
 characterList.append("RASHAAD")
- 
+
+# for each page in the pdf
 for i in range(pdfReader.numPages):
+    # create a list of all of the words on that page
     pageObj = pdfReader.getPage(i)
     words = pageObj.extract_text()
     words = words.split(' ');
+    # print the page number
     print()
     print()
     print(i)
+    # create a shallow copy of the character list
     pageCharacterList = []
     for character in characterList:
         pageCharacterList.append(character)
-    for j in words:
-        for k in pageCharacterList:
-            if k in j:
-                print(k)
-                pageCharacterList.remove(k)
+    for j in words: # for each word on the page
+        for k in pageCharacterList: # compare each word with each character
+            if k in j: # if the word is a character in the list
+                print(k)    # print the character name
+                pageCharacterList.remove(k)   # remove the name from the shallow list copy
 
 # closing the pdf file object
 pdfFileObj.close()
